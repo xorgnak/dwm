c8a95856edd3e53f306c0f0226035d9237453187
Max Catman <xorgnak@gmail.com>
2020-12-04 19:34:23 -0500

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,6 +1,7 @@
 
-
-sudo apt install -y suckless-tools stterm libxinerama-dev libxft-dev xinit xorg xterm cromium-browser lxterminal
+sudo apt update
+sudo apt upgrade
+sudo apt install -y suckless-tools stterm libx11-dev libxinerama-dev libxft-dev xinit xorg xterm cromium-browser lxterminal
 
 sudo make clean install
 for u in /home/*
